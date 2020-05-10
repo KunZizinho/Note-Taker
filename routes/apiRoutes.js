59639ad585d5e926be055247d73eda7a0857c4d7
Mario Kozic <kun.zizinho@gmail.com>
2020-05-09 23:54:20 -0400

    added some code to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,3 +2,46 @@ const fs = require("fs")
 const path = require("path");
 
 var noteData;
+
+module.exports = function (app) {
+    fs.readFile("db.json", "utf8", (err, data) =>{
+        if(err) throw err;
+        notedata = json.parse(data);
+        console.log("here")
+    })
+
+    app.get("/api/notes", (req, res) => {
+        res.json(noteData);
+        console.log(noteData);
+    });
+
+    app.post("/api/notes", (req, res) => {
+        console.log("test here")
+        var newNotes = req.body;
+        let parseData = JSON.stringify(noteData);
+        fs.writeFile(path.join(db.json), parseData, err =>{
+            if(err) throw err;
+
+        })
+        res.json(noteData)
+    })
+
+    app.delete("/api/notes/:id", (req, res) => {
+
+        var deleteData = req.params.id;
+        console.log(deleteData)
+        for(var i = 0; i < noteData.length; i++){
+            if(deleteData === noteData[i].title){
+                noteData.splice(i, 1)
+            };
+        };
+
+        let parseData = JSON.stringify(noteData);
+        fs.writeFile(path.join("db.json"), parseData, err => {
+            if(err) throw err;
+
+        })
+        console.log(noteData)
+        res.json(noteData);
+    })
+}
