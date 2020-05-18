b3b6b7c7c5c85921f1a39018f4fe44fa1938ee75
KunZizinho <kun.zizinho@gmail.com>
2020-05-18 00:10:01 -0400

    working on adding notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,18 +4,18 @@ const path = require("path");
 var noteData;
 
 module.exports = function (app) {
-    fs.readFile("../db/db.json", "utf8", (err, data) =>{
+    fs.readFile("./db/db.json", "utf8", (err, data) =>{
         if(err) throw err;
         noteData = JSON.parse(data);
         console.log("here")
     })
 
-    app.get("/api/notes", (req, res) => {
+    app.get("./public/notes.html", (req, res) => {
         res.json(noteData);
         console.log(noteData);
     });
 
-    app.post("/api/notes", (req, res) => {
+    app.post("./public/notes", (req, res) => {
         console.log("test here")
         var newNotes = req.body;
         let parseData = JSON.stringify(noteData);
@@ -37,7 +37,7 @@ module.exports = function (app) {
         };
 
         let parseData = JSON.stringify(noteData);
-        fs.writeFile(path.join("../db/db.json"), parseData, err => {
+        fs.writeFile(path.join("./db/db.json"), parseData, err => {
             if(err) throw err;
 
         })
